7a6ad68d7ebafe62bb4b1eee6f7f09bf9662a0f5
salihsefer36 <138879981+salihsefer36@users.noreply.github.com>
2025-01-26 17:11:09 +0300

    Summation
    Summation for those two binary digits

--- binaryDigits.c
+++ binaryDigits.c
@@ -3,23 +3,9 @@
 #include <math.h>
 
 int bit_count;
-char binary_input[65];
+char binary_input1[65], binary_input2[65];
 
-void CheckAndPrintBinaryNumber()
-{
-    printf("Please enter your number's bit: ");
-    scanf("%d", &bit_count);
-
-    printf("Please enter %d bit binary number : ", bit_count);
-    scanf("%s", binary_input);
-
-    int input_length = strlen(binary_input);
-
-    if (input_length != bit_count) {
-        printf("Error: The binary number you entered does not fit within the specified length of %d bits\n", bit_count);
-        return;
-    }
-
+int ConvertBinaryToDecimal(const char* binary_input, int bit_count) {
     int decimal_value = 0;
     int is_negative = (binary_input[0] == '1');
 
@@ -29,10 +15,9 @@ void CheckAndPrintBinaryNumber()
                 decimal_value += (int)pow(2, bit_count - 1 - i);
             } else if (binary_input[i] != '0') {
                 printf("Error: The binary number you entered is not valid\n");
-                return;
+                return 0;
             }
         }
-
         decimal_value -= (int)pow(2, bit_count);
     } else {
         for (int i = 0; i < bit_count; i++) {
@@ -40,17 +25,45 @@ void CheckAndPrintBinaryNumber()
                 decimal_value += (int)pow(2, bit_count - 1 - i);
             } else if (binary_input[i] != '0') {
                 printf("Error: The binary number you entered is not valid\n");
-                return;
+                return 0;
             }
         }
     }
 
-    printf("Your binary number: %s\n", binary_input);
-    printf("Converted to decimal number: %d\n", decimal_value);
+    return decimal_value;
 }
 
-int main() 
-{
-    CheckAndPrintBinaryNumber();
+void CheckAndPrintBinarySum() {
+    printf("Please enter your number's bit count: ");
+    scanf("%d", &bit_count);
+
+    printf("Please enter the first %d-bit binary number: ", bit_count);
+    scanf("%s", binary_input1);
+
+    if (strlen(binary_input1) != bit_count) {
+        printf("Error: The binary number you entered does not fit within the specified length of %d bits\n", bit_count);
+        return;
+    }
+
+    printf("Please enter the second %d-bit binary number: ", bit_count);
+    scanf("%s", binary_input2);
+
+    if (strlen(binary_input2) != bit_count) {
+        printf("Error: The binary number you entered does not fit within the specified length of %d bits\n", bit_count);
+        return;
+    }
+
+    int decimal1 = ConvertBinaryToDecimal(binary_input1, bit_count);
+    int decimal2 = ConvertBinaryToDecimal(binary_input2, bit_count);
+
+    int sum = decimal1 + decimal2;
+
+    printf("First binary number: %s\n", binary_input1);
+    printf("Second binary number: %s\n", binary_input2);
+    printf("Sum in decimal: %d\n", sum);
+}
+
+int main() {
+    CheckAndPrintBinarySum();
     return 0;
 }
